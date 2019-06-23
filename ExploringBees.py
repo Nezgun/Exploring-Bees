f87195455b55b30487d04e4a7fbb003d7f9e46a6
Nezgun <MHoffman.cheb@gmail.com>
2019-06-23 16:22:29 -0400

    Set up and tested Tile class.  Should be good to go

--- ExploringBees.py
+++ ExploringBees.py
@@ -1,3 +1,6 @@
-import MapGeneration
-import Hive
+from WorldGen.Tile import Tile
 
+tile1 = Tile(5, 5)
+tile2 = Tile(8, 9)
+
+print(tile1.distance(tile2))
---
+++ WorldGen/Tile.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 18 14:34:29 2019
+
+"""
+import random
+import math
+
+class Tile:
+    def __init__(self, x, y):
+        self.xCoord = x
+        self.yCoord = y
+
+        #Is this tile a food source?
+        if random.random() > 0.90:
+            self.foodSource = True
+            self.availableFood = int(random.gauss(50, 10))
+            self.fragrance = float(self.availableFood)
+        else:
+            self.foodSource = False
+            self.availableFood = 0
+            self.fragrance = 0.00
+
+    #Getters
+    def getX(self):
+        return self.xCoord
+
+    def getY(self):
+        return self.yCoord
+
+    def isFoodSource(self):
+        return self.foodSource
+
+    def getFood(self):
+        return self.availableFood
+
+    def getFragrance(self):
+        return self.fragrance
+
+    def getCoordinates(self):
+        return self.xCoord, self.yCoord
+
+    #Modifiers
+
+    #adds to fragrance.
+    def increaseFragrance(self, val):
+        self.fragrance += val
+
+    #removes from fragrance (could be done by passing a negative value, but this is more clear I feel)
+    def reduceFragrance(self, val):
+        self.fragrance -= val
+
+    #Reduces food in the tile.
+    def foodReduce(self):
+        self.availableFood += 1
+        if self.availableFood <= 0:
+            self.foodSource = False
+
+    #Interactors
+    #Adds fragrance to nearby tiles Circules around self using rate of Fragrance * (1/r^2)
+    def addFragrance(destination):
+        return
+
+    def distance(self, destination):
+        x1 = self.xCoord
+        y1 = self.yCoord
+        x2, y2 = destination.getCoordinates()
+        return math.sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2)) 
+
+    #Display
+    def displayInfo(self):
+        print("Internal Information:")
+        print("Coordinates: (" + str(self.xCoord) + ", " + str(self.yCoord) + ")")
+        print("Food Source: " + str(self.foodSource))
+        print("Available Food: " + str(self.availableFood))
+        print("Fragrance: " + str(self.fragrance))
+
+    #OVERRIDES
+
+    def print(self):
+        print(self.availableFood)
