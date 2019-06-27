398a2124f0a543665a560ccda8a9943cc7936582
Nezgun <MHoffman.cheb@gmail.com>
2019-06-27 11:58:57 -0400

    set up Bee framework

--- Bee.py
+++ Bee.py
@@ -2,4 +2,37 @@
 
 class Bee:
     def __init__(self):
+        #affiliation
+        carriedFood = 0
+        #importantMemory
+        #genericMemory
+        #hiveMemory
+
+        return
+
+    def explore():
+        return
+
+    def pinpoint():
+        return
+
+    def alert():
+        return
+
+    def move():
+        return
+
+    def action():
+        return
+
+    def gather():
+        return
+
+    def updateHive():
+        return
+
+    def updateSelf():
+        return
+
+    def remember():
         return
