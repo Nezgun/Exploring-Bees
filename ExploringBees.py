ca8597ebe4878f462f1f226a5b6fb1663d7b83bf
Nezgun <MHoffman.cheb@gmail.com>
2019-07-03 02:33:12 -0400

    shifted some functions around + added more file structure for future additions

--- ExploringBees.py
+++ ExploringBees.py
@@ -1,17 +1,3 @@
 from mainFunctions import *
-from WorldGen.World import World
 
-#Manual or Auto Setup
-numOfWorlds = int(input("How man worlds would you like to initialize?\n"))
-size = input("Select a map size: small/medium/large\n")
-
-displayOverview(size, numOfWorlds)
-
-size = sizeSelection(size)
-worldList = []
-for i in range(numOfWorlds):
-    worldList.append(World(size))
-
-print(displayWorldsCondensed(worldList))
-
-saveWorld(worldList)
+execute()
--- mainFunctions.py
+++ mainFunctions.py
@@ -1,8 +1,5 @@
 from WorldGen.World import World
 
-def worldInitialization(size = 1000):
-    return World(size)
-
 def displayOverview(size, numOfWorlds):
     print("Initalization Overview")
     print(size + " size selected.")
@@ -33,3 +30,59 @@ def saveWorld(worldList):
     newFile = open(r"worldData.txt", "w")
     newFile.write(displayWorldsCondensed(worldList))
     newFile.close()
+
+def autoSetup():
+    numOfWorlds = 1
+    size = "medium"
+    return numOfWorlds, size
+
+def manualSetup():
+    numOfWorlds, size = setParameters()
+    return numOfWorlds, size
+
+def setParameters():
+    numOfWorlds = int(input("How many worlds would you like to initialize?\n"))
+    size = input("Select a map size: small/medium/large\n")
+    return numOfWorlds, size
+
+def setup():
+    setup = int(input("Choose manual setup (0) or automatic setup (1):\n"))
+    if setup == 0:
+        return "manual"
+    elif setup == 1:
+        return "auto"
+    else: #should never occur
+        return 0
+
+def initalizeWorlds(numOfWorlds, size):
+    worldList = []
+    for i in range(numOfWorlds):
+        worldList.append(World(size))
+    return worldList
+
+def displayWorlds(worldList):
+    for world in worldList:
+        world.displayCondensed()
+
+def sizeConversion(size):
+    if size == "small":
+        return 500
+    elif size == "medium":
+        return 1000
+    elif size == "large":
+        return 2000
+    else:
+        return 0
+
+def execute():
+    setupSelection = setup()
+    if setupSelection == "manual":
+        numOfWorlds, size = manualSetup()
+    elif setupSelection == "auto":
+        numOfWorlds, size = autoSetup()
+
+    worldList = initalizeWorlds(numOfWorlds, sizeConversion(size)) #array of worlds involved in simulation
+
+    displayWorlds(worldList)
+
+    return
